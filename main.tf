aa1b82a36d1d8a9c9fd8a493f9c7f06e0ac82184
Altaf Khan <Akhan8508@conestogac.on.ca>
2025-07-31 22:30:01 -0400

    rechecked and resubmitted

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI
+  ami           = "ami-08a6efd148b1f7504" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   count         = var.core_count
   tags = {
