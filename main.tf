eb7d56fd21bdc31f9f4bfa25adf28d90dd6d4978
Altaf Khan <Akhan8508@conestogac.on.ca>
2025-07-31 21:47:54 -0400

    changes created into file

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "value"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+  }
+}
 provider "aws" {
   region = "us-east-1"
+
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   count         = var.core_count
+  tags = {
+    Name = "ExampleInstance"
+  }
 }
