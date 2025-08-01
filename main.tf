8f8de252b00acfd060d95d462fb21d3e8e8da187
Altaf Khan <Akhan8508@conestogac.on.ca>
2025-07-31 21:33:48 -0400

    Added Terraform files and Workflows

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"  # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  count         = var.core_count
+}
