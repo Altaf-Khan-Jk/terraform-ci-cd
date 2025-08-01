fac7c80360672fc2d42ffe91ebcee4b467b8be31
Altaf Khan <Akhan8508@conestogac.on.ca>
2025-07-31 21:42:46 -0400

    updated file

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"  # Amazon Linux 2 AMI
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   count         = var.core_count
 }
